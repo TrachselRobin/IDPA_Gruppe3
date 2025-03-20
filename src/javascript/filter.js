c4fe4fe562243ce0e7703c981d1c7a5d8072524f
krausm-bzz <krausm@bzz.ch>
2025-03-20 14:42:22 +0100

    filter basics

--- src/javascript/filter.js
+++ src/javascript/filter.js
@@ -1,6 +1,21 @@
 function filter(data) {
-    // TODO
-    console.log("filter")
+    return data.stationboard.map(train => {
+        const departure_time = new Date(train.stop.departure)
 
-    return data
+        const hinweise = [
+            train.stop.delay > 0 ? `+${train.stop.delay}'` : '',
+            train.stop.prognosis.platform && train.stop.prognosis.platform !== train.stop.platform
+                ? `Gleiswechsel: Vom Gleis ${train.stop.platform} zu Gleis ${train.stop.prognosis.platform}`
+                : ''
+        ].filter(Boolean).join(', ');
+
+        return {
+            linie: train.number,
+            ziel: train.to,
+            gleis: train.stop.platform,
+            abfahrtszeit: departure_time.toLocaleTimeString(),
+            hinweise: hinweise || ''
+        };
+    })
 }
+
