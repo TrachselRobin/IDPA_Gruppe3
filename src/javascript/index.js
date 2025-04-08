ce827e938d8411429d5662bf61c2bb4528297722
trachselr <trachselr@bzz.ch>
2025-04-08 19:05:28 +0200

    ADDED: testdata, if api fails in test phase

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -5,6 +5,73 @@ const STATION  = "Horgen" // anpassbar (vielleicht Cookies); geoposition abfrage
 const INTERVAL = 30000
 const LIMIT    = 3
 
+const test_data = [
+    {
+        "category": "S",
+        "line": "S8",
+        "destination": "Pfäffikon SZ",
+        "departure_time": "19:00",
+        "arrival_time": "19:19",
+        "platform": "4",
+        "stops": [
+            "Au ZH",
+            "Wädenswil",
+            "Richterswil",
+            "Bäch SZ",
+            "Freienbach SBB",
+            "Pfäffikon SZ"
+        ],
+        "delay": 1,
+        "info": "Verspätung: +1 Min"
+    },
+    {
+        "category": "S",
+        "line": "S8",
+        "destination": "Winterthur",
+        "departure_time": "19:00",
+        "arrival_time": "19:49",
+        "platform": "3",
+        "stops": [
+            "Oberrieden",
+            "Thalwil",
+            "Rüschlikon",
+            "Kilchberg ZH",
+            "Zürich Wollishofen",
+            "Zürich Enge",
+            "Zürich Wiedikon",
+            "Zürich HB",
+            "Zürich Oerlikon",
+            "Wallisellen",
+            "Dietlikon",
+            "Effretikon",
+            "Winterthur"
+        ],
+        "delay": 0,
+        "info": "Keine besonderen Hinweise"
+    },
+    {
+        "category": "S",
+        "line": "S2",
+        "destination": "Ziegelbrücke",
+        "departure_time": "19:05",
+        "arrival_time": "19:44",
+        "platform": "4",
+        "stops": [
+            "Wädenswil",
+            "Richterswil",
+            "Pfäffikon SZ",
+            "Altendorf",
+            "Lachen SZ",
+            "Siebnen-Wangen",
+            "Schübelbach-Buttikon",
+            "Reichenburg",
+            "Ziegelbrücke"
+        ],
+        "delay": 1,
+        "info": "Verspätung: +1 Min"
+    }
+]
+
 let last_data  = null;
 
 async function reload_data() {
