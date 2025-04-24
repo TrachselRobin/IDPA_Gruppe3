f332019c77f4de9d41067d34563fb070b5d9e70a
trachselr <trachselr@bzz.ch>
2025-04-24 12:46:17 +0200

    UPDATED: smoothness of scroll

--- src/javascript/scroll.js
+++ src/javascript/scroll.js
@@ -37,11 +37,11 @@ const scroll = async (duration) => {
 // Führt die eigentliche Scrollbewegung durch
 const scrollToEnd = async (elementList, timePerDot) => {
     const containers = Array.from(elementList);
-    const smoothness = 100;
+    const smoothness = 50;
 
     const maxScroll = Math.max(...containers.map(el => el.scrollWidth - el.clientWidth));
     const maxStops = Math.max(...containers.map(el => el.querySelectorAll(".punkt").length));
-    const steps = (maxStops + 5) * smoothness;
+    const steps = (maxStops + 1) * smoothness;
 
     if (steps <= 0) return;
 
